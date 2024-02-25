43a57ca0c7cc33084289e23bef81059d8397104a
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-02-25 14:57:27 -0800

    Fix ourput role values
    The output variables was called role_name but in fact it returned a profile name. Fixed.
    In case a user need the profile - those variables are available in addition to role name/ARN.
    Export jumphost hostname. Sometimes, it's handy.

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,26 @@
 output "jumphost_role_arn" {
   description = "Instance IAM role ARN."
-  value       = module.jumphost_profile.instance_profile_arn
+  value       = module.jumphost_profile.instance_role_arn
 }
 output "jumphost_role_name" {
   description = "Instance IAM role name."
+  value       = module.jumphost_profile.instance_role_name
+}
+
+output "jumphost_instance_profile__arn" {
+  description = "Instance IAM profile ARN."
+  value       = module.jumphost_profile.instance_profile_arn
+}
+output "jumphost_instance_profile_name" {
+  description = "Instance IAM profile name."
   value       = module.jumphost_profile.instance_profile_name
 }
 
 output "jumphost_asg_name" {
-  value = aws_autoscaling_group.jumphost.name
+  description = "Jumphost autoscaling group"
+  value       = aws_autoscaling_group.jumphost.name
+}
+
+output "jumphost_hostname" {
+  value = "${var.route53_hostname}.${data.aws_route53_zone.jumphost_zone.name}"
 }
