bad836353498198841c7673391d12459e72a2f24
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-06-25 16:16:20 -0700

    Use echo service as health check (#23)
    SSH service starts immediately, even before puppet successfully runs.
    xinetd starts echo service on TCP port 7. It starts after puppet apply
    is put into the cron.

--- security_group.tf
+++ security_group.tf
@@ -23,6 +23,21 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   )
 }
 
+resource "aws_vpc_security_group_ingress_rule" "echo" {
+  description       = "Allow NLB healthchecks"
+  security_group_id = aws_security_group.jumphost.id
+  from_port         = 7
+  to_port           = 7
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+  tags = merge(
+    {
+      Name = "Echo access"
+    },
+    local.tags
+  )
+}
+
 resource "aws_vpc_security_group_ingress_rule" "icmp" {
   description       = "Allow all ICMP traffic"
   security_group_id = aws_security_group.jumphost.id
@@ -30,8 +45,9 @@ resource "aws_vpc_security_group_ingress_rule" "icmp" {
   to_port           = -1
   ip_protocol       = "icmp"
   cidr_ipv4         = "0.0.0.0/0"
-  tags = merge({
-    Name = "ICMP traffic"
+  tags = merge(
+    {
+      Name = "ICMP traffic"
     },
     local.tags
   )
