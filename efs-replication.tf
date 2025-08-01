36289207305f0f394672b8637a55afc7976f3306
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-08-01 16:39:22 -0700

    Use the encrypted volume but keep unecrypted

--- efs-replication.tf
+++ efs-replication.tf
@@ -1,7 +1,7 @@
-resource "aws_efs_replication_configuration" "home" {
-  source_file_system_id = aws_efs_file_system.home.id
-  destination {
-    region         = data.aws_region.current.name
-    file_system_id = aws_efs_file_system.home-enc.id
-  }
-}
+# resource "aws_efs_replication_configuration" "home" {
+#   source_file_system_id = aws_efs_file_system.home.id
+#   destination {
+#     region         = data.aws_region.current.name
+#     file_system_id = aws_efs_file_system.home-enc.id
+#   }
+# }
