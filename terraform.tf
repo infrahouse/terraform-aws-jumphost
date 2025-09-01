8aae65654fe2da190120ff6ed991d501f5d48526
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-09-01 14:05:58 -0700

    Add support for AWS provider version 6 with parametrized tests
    - Add pytest parametrize decorator to test both AWS provider ~> 5.31 and ~> 6.0
    - Update AWS provider version constraint to >= 5.31, < 7.0 in main terraform.tf
    - Remove migration test

--- terraform.tf
+++ terraform.tf
@@ -3,15 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.31"
-    }
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = ">= 2.3"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.2"
+      version = ">= 5.31, < 7.0"
     }
     random = {
       source  = "hashicorp/random"
--- test_data/jumphost-2.9/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "role_arn" {
-  default = null
-}
-variable "test_zone" {}
-variable "region" {}
-
-variable "nlb_subnet_ids" {}
-variable "asg_subnet_ids" {}
