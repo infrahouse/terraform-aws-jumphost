e0a0b698faea8ef9c1d90511fba2fde97e10e35d
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2026-07-03 12:40:25 -0700

    chore: upgrade dependencies, cleanup the unused variables

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,6 @@ import logging
 from infrahouse_core.logging import setup_logging
 from pytest_infrahouse import LOG as IH_LOG
 
-# "303467602807" is our test account
-TEST_ACCOUNT = "303467602807"
-# TEST_ROLE_ARN = "arn:aws:iam::303467602807:role/jumphost-tester"
-DEFAULT_PROGRESS_INTERVAL = 10
-UBUNTU_CODENAME = "jammy"
 TERRAFORM_ROOT_DIR = "test_data"
 
 LOG = logging.getLogger(__name__)
